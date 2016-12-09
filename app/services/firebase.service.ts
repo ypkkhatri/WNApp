fe8c2dadddeb4bda6b58280f9cdd0a6ebe3907fb
Yougeshwar Khatri <ypkkhatri@gmail.com>
2016-12-09 11:52:27 +0500

    Firebase init of role

--- app/services/firebase.service.ts
+++ app/services/firebase.service.ts
@@ -8,21 +8,32 @@ declare var firebase: any;
 export class FirebaseService {
 
   constructor(private session: SessionService, private router: Router) {
+    this.init();
+  }
 
+  init(): void {
+    if (this.session.isLoggedIn()) {
+      firebase.database().ref('/roles/Z9tlmo1fs0cBIGBY02WW4P4IYTC2/').set({
+        name: "admin"
+      });
+      firebase.database().ref('/roles/z3QCR9v3bDe7auIrlhV8GqymaH73/').set({
+        name: "user"
+      });
+    }
   }
 
   signupUser(email: string, password: string) {
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error: any){
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error: any) {
       console.log(error);
     });
   }
 
   signInWithEmail(email: string, password: string): void {
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error: any) {
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error: any) {
       console.log(error);
     });
     this.loadUser();
-    if(this.session.isLoggedIn()) {
+    if (this.session.isLoggedIn()) {
       this.router.navigate(['/home']);
     }
   }
